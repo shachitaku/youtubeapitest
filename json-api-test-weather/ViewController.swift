1e5302ae5d3d40552bfb2aa19207d92d9cb475d1
staff birdie <staff@sutaffunomacbook-pro.local>
2018-11-13 16:19:00 +0900

    Initial Commit

---
+++ json-api-test-weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  json-api-test-weather
+//
+//  Created by staff on 2018/11/13.
+//  Copyright © 2018 takumi-kimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
